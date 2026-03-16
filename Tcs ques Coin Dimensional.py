f7c0124ce8e52bdaa9b276bd03e425ad222fadb3
Kaviyapari <kaviyapari1916@gmail.com>
2026-03-16 14:37:45 +0530

    Add files via upload

---
+++ Tcs ques Coin Dimensional.py
@@ -0,0 +1,16 @@
+amount=int(input("Enter a Amount in Rs:"))
+coins=[500,200,100,50,20,10,5,2,1]
+for i in coins:
+    if(i<=amount):
+        print(i,end=" ")
+        amount=amount-i
+        
+    
+        
+        
+        
+        
+                        
+
+
+ 
