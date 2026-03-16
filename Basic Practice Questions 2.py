2e041277364b6a02bae3a490abcba3ce2636fb93
Kaviyapari <kaviyapari1916@gmail.com>
2026-03-16 13:20:27 +0530

    Add files via upload

---
+++ Basic Practice Questions 2.py
@@ -0,0 +1,52 @@
+for i in range(5):
+    print(i)
+    
+for i in range(5):
+    print("Hii")
+    
+for i in range(100,110):
+    print(i)
+    
+for i in range(2,20,3):
+    print(i+10)
+    
+for i in range(10,20,2):
+    print(i)
+
+for i in "CSE":
+    print("ECE")
+    
+for i in "Iloveindia":
+    if i in "AEIIUaliou":
+        print(i)
+        
+a=10
+for i in range(7):
+    a=a+2
+print(a)  
+
+a=['Kaviya','Ramya','Sangavi']
+s=1
+for i in range(3):
+     print("{}.{}".format(s,a[i]))
+     s=s+1
+     
+for i in range(5):
+    for j in range(4):
+        print(i)
+    print()
+    
+for i in range(5):
+    for j in range(4):
+        print(i+j)
+    print()   
+    
+for i in range(5):
+    for j in range(i+2):
+        print("hai")
+    print()
+     
+    
+    
+    
+    
